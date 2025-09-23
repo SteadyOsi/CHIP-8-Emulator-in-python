814362c94a0199fd2638ffe51a4c3afd8952fcaf
SteadyOsi <Josiahstedman@gmail.com>
2025-09-23 12:25:09 +1000

    Reading binary
    started opening a file and reading the binary in it

---
+++ readBinary_test.py
@@ -0,0 +1,7 @@
+with open("/home/minion/Documents/GitHub/CHIP8-Roms/chip8-roms/demos/Particle Demo [zeroZshadow, 2008].ch8", "rb") as file:
+
+    data = file.read(1)
+
+print(data)
+print(type(data))
+
