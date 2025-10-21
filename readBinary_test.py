86ddd1452de0eca2dab2db7cd547744a9c6b8822
SteadyOsi <Josiahstedman@gmail.com>
2025-10-21 17:36:05 +1100

    lecture 1
    created main system stuff like memory v0-vf PC, was able to load a rom into meory and do a santity check

--- readBinary_test.py
+++ readBinary_test.py
@@ -1,7 +1,26 @@
-with open("/home/minion/Documents/GitHub/CHIP8-Roms/chip8-roms/demos/Particle Demo [zeroZshadow, 2008].ch8", "rb") as file:
+rom_Address = "CHIP8-Roms/chip8-roms/programs/IBM Logo.ch8"
 
-    data = file.read(1)
+def whats_In_Mem(mem): # still in works
+    for i in mem:
+        print(i)
 
-print(data)
-print(type(data))
+memory = bytearray(4096)
+PC = 0x200
+I = 0
+V = [0] * 16
 
+with open(rom_Address, "rb") as rom_file:
+    rom_data = rom_file.read()
+
+if len(rom_data) > 4096 - 0x200:
+    print("Rom is too large")
+
+for i,byte in enumerate(rom_data):
+    memory[0x200 + i] = byte
+
+i = 0
+while(i < 8):
+    print(f"memory address {hex(PC + i)} : {hex(memory[PC + i])}")
+    i += 1
+
+print(f"value of PC {hex(PC)}")
