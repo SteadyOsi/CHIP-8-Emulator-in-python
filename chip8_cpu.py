ee0460ca19d81065c43718e2b211e9adb3e55ec0
SteadyOsi <Josiahstedman@gmail.com>
2025-12-02 18:46:38 +1100

    CPU
    Created two new files.
    One being the CPU file the other being the "main" file. CPU file now has a CPU shell and some defined functions

---
+++ chip8_cpu.py
@@ -0,0 +1,34 @@
+class Chip8_CPU:
+    def __init__(self):
+        self.memory = bytearray(4096)  # CHIP-8 has 4KB of addressable memory
+        self.PC = 0x200  # programs start at 0x200; below holds font sprites/reserved space
+        self.I = 0  # index register (unused in this simple loader)
+        self.V = [0] * 16  # general-purpose registers V0-VF
+        self.SP = 0 #stack pointer 
+        self.DT, self.ST = 0 # display and sound timers (tick @ 60 Hz)
+        self.display = [[False for _ in range(64)]for _ in range(32)] # sets a 2D list to all false, its Y,X
+        self.keys = [False for _ in range(16)]
+        self.draw_Dirty = False 
+
+    def reset(self):
+        self.memory = bytearray(4096)  # CHIP-8 has 4KB of addressable memory
+        self.PC = 0x200  # programs start at 0x200; below holds font sprites/reserved space
+        self.I = 0  # index register (unused in this simple loader)
+        self.V = [0] * 16  # general-purpose registers V0-VF
+        self.SP = 0 #stack pointer 
+        self.DT, self.ST = 0 # display and sound timers (tick @ 60 Hz)
+        self.display = [[False for _ in range(64)]for _ in range(32)] # sets a 2D list to all false, its Y,X
+        self.keys = [False for _ in range(16)]
+        self.draw_Dirty = False 
+
+    def fetch(self): # returns 4 bytes based on the current PC
+        FirstPart = self.memory[self.PC]
+        SecondPart = self.memory[self.PC + 1]
+        return (FirstPart << 8) | SecondPart
+
+
+    def increment(self): # increments by 2 bytes 
+        self.PC += 2
+    
+    def peek(self): # peek just at the first 2 bytes
+        return hex(self.memory[self.PC])
---
+++ main.py
@@ -0,0 +1,8 @@
+import chip8_cpu as chip #
+
+
+
+cpu = chip.Chip8_CPU()
+
+cpu.peek()
+
